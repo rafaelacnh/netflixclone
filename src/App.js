c4348556d3118d5c390a5dbe0e15fbfc1961334e
rafaela <rafaela.cunha@calindra.com.br>
2021-05-16 23:45:58 -0300

    salvando e exibindo lista criada

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React , {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb'
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([])
 
     useEffect(() => {
     const loadAll = async () => {//funcao carregar tudo
       //pegando lista total
       let list = await Tmdb.getHomeList()//
+      setMovieList(list)
       console.log(list);
+
     }
     loadAll()
   }, [])
