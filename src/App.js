ca14ec9518dd408cd4e823f3ccf1d0469b60d0ba
rafaela <rafaela.cunha@calindra.com.br>
2021-05-16 23:16:02 -0300

    iniciando app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React  from 'react';
+
+export default () => {
+
+
+  return (
+    <div>
+      <h1>OLá Mundo!</h1>
+    </div>
+  );
+}
