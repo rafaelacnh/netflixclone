244cfaa8ea1760755c0c41fc69adf694b57aa21e
rafaela <rafaela.cunha@calindra.com.br>
2021-05-17 22:27:07 -0300

    criando requisicao buscando type filme pelo movieId

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -3,6 +3,11 @@ import './FeaturedMovie.css';
 
 export default ({item}) => {
     return (
-       
+        <section className="featured" style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage:`url(https://image.tmdb.org/t/p/original${item.backdrop_path})`}}>
+          
+        </section>
     )
 }
