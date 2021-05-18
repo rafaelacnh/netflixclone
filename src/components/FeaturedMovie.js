024f12469ddf1227969d697662b33cc3c43554de
rafaela <rafaela.cunha@calindra.com.br>
2021-05-17 22:38:46 -0300

    exibindo data manipulando Date

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './FeaturedMovie.css';
 
-export default ({item}) => {
+export default ({ item }) => {
+    console.log(item);
+
+    let firstDate = new Date(item.first_air_date);
+
     return (
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -12,7 +16,7 @@ export default ({item}) => {
                     <div className="featured--name">{item.original_name}</div>
                     <div className="featured--info">
                         <div className="featured--points">{item.vote_average}pontos</div>
-                        <div className="featured--year">2009</div>
+                        <div className="featured--year">{firstDate.getFullYear()}</div>
                         <div className="featured--seasons">{item.number_of_seasons}temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                         <div className="featured--description">{item.overview}</div>
                         <div className="featured--buttons"></div>
